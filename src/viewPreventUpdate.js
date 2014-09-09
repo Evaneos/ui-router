b2bb2dad8842b95fecbc673affb6cbb65e403487
Robin Komiwes <robin.komiwes@gmail.com>
2014-09-09 17:19:20 +0200

    Correctly exposing toState and fromState

--- src/viewPreventUpdate.js
+++ src/viewPreventUpdate.js
@@ -26,7 +26,9 @@ function $ViewPreventUpdateProvider() {
 angular.module('ui.router.state').provider('$viewPreventUpdate', $ViewPreventUpdateProvider).run(['$rootScope', '$viewPreventUpdate', function($rootScope, $viewPreventUpdate) {
   $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
     $viewPreventUpdate.toParams   = toParams;
+    $viewPreventUpdate.toState    = toState;
     $viewPreventUpdate.fromParams = fromParams;
+    $viewPreventUpdate.fromState  = fromState;
   });
 
   $rootScope.$on('$viewResolving', function(event, name) {
